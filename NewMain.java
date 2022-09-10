3cf5c042c2df38cd75bd6fe477142da7d603c650
ShreyaPatel20063 <sap20063@gmailcom>
2022-09-10 15:35:50 +0530

    Initial Commit

---
+++ NewMain.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginWindow_1 lw = new LoginWindow_1();
+        Login_SignUp ls = new Login_SignUp();
+        Fdeposit fd = new Fdeposit();
+        Fwithdraw fw = new Fwithdraw();
+    }
+    
+}
